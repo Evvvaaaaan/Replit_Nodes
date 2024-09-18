e00fc6752f9cb43316db0b3aad128b4d934ca42d
Evvvaaaaan <110900656+Evvvaaaaan@users.noreply.github.com>
2024-09-18 05:09:13 +0000

    add comment

--- scraping.py
+++ scraping.py
@@ -1,17 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 
+# 사이트의 html을 가져오기 위해 url을 입력함 
 url = "https://weworkremotely.com/categories/remote-full-stack-programming-jobs"
 
+# request 패키지를 사용하여 url에 get요청을 보냄
 response = requests.get(url)
 
+#html.parser를 사용해서 soup에 넣음
 soup = BeautifulSoup(response.content, "html.parser",)
 
+#section, class="jobs" 모든 li들을 찾아서 jobs에 저장
 jobs = soup.find("section", class_="jobs").find_all("li")[1:-1]
 
 for job in jobs:
   title = job.find("span", class_="title").text
- 
   company, position, region = job.find_all("span", class_="company")
   company = company.text
   position = position.text
